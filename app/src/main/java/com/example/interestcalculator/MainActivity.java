9a4f6b4078ea6fa8a185564df48f90b7ee4316cb
w0rzz <35713699+w0rzz@users.noreply.github.com>
2020-06-18 07:09:46 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/interestcalculator/MainActivity.java
+++ app/src/main/java/com/example/interestcalculator/MainActivity.java
@@ -10,5 +10,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        findViewById(R.id.tv).setVisibility(View.INVISIBLE);
+        
+        EditText edtAmount, edtPeriod;
+        Button bt;
+        
+        int period;
+        double totalRepayment;
+        double interestRate = 10;
+    }
+    
+    edtAmount = findViewById(R.id.EditTv1);
+    edtPeriod = findViewById(R.id.EditTv2);
+    button = findViewById(R.id.bt);
+    
+    public void buttonClicked(View v)
+    {
+        calculateInterest();
+        findViewById(R.id.tv).setText(totalRepayment.toString());
+        findViewById(R.id.tv).setVisibility(View.VISIBLE);
+    }
+
+    public void calculateInterest()
+    {
+        months = Integer.parseInt(edtPeriod.getText()) * 12;
+
+        double oprt = 1 + (interestRate * months);
+
+    //    double monthlyTop = interestRate * pow((1 + interestRate), months);
+    //    double monthlyBottom = pow(1 + interestRate, months) - 1;
+
+        totalRepayment = Double.parseDouble(editAmount.getText()) * oprt;
+   
     }
 }
+
